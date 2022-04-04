4e968bf17ebc6b0923b1faabd3a1f95dfe15e520
João Henrique Ferreira <93941666+J-Henrique-Ferreira@users.noreply.github.com>
2022-04-04 20:23:00 -0300

    att codes

--- codejs/codeSplitter.js
+++ codejs/codeSplitter.js
@@ -1,158 +1,194 @@
+let dripPercentage = 0;
+
+//obtem o valor da conta mais a porcentagem da gorjeta sobre a conta
 function splitter(dripPercentage) {
     let inBill = document.getElementById("inBill");
     let bill = Number(inBill.value);
 
-    let amount = bill + dripPercentage * bill;
+    let amount = bill + (dripPercentage * bill);
 
-    return amount;
+    return amount;      // retorna o valor da conta mais gorjeta
 }
 
+
+//------------------
+
+//verifica se o campo conta foi preenchido corretamente
 function verify() {
+    //obtem o valor do input
     let inBill = document.getElementById("inBill");
+    let inpPeople = document.getElementById("inpPeople");
+
     let bill = Number(inBill.value);
+    let people = Number(inpPeople.value);
+
+    let alertBill = document.getElementById("alertBill");
+    let alertPercentage = document.getElementById("alertPercentage");
+    let alertPeople = document.getElementById("alertPeople");
+
+    let btReset = document.getElementById("btReset");
+
+    //se valor for invalido retorna um alert e direciona o foco do cursor
     if (bill == "" || bill == isNaN(bill)) {
-        alert("Fill in the account amount.")
+        alertBill.className = "exibe";
         inBill.focus();
+        return;
+    } else {
+        alertBill.className = "oculta";
+    }
+
+    if (dripPercentage == 0) {
+        alertPercentage.className = "exibe";
+        alertPercentage.focus();
+        return;
+    } else {
+        alertPercentage.className = "oculta";
+        inpPeople.focus();
+    }
+
+    if (people == "" || people == isNaN(people)) {
+        alertPeople.className = "exibe";
+        return;
+    } else {
+        alertPeople.className = "oculta";
+        btReset.focus();
     }
 }
 
+
+
+
+//------------------
+
+//evento click no bt5
 function clickbt5() {
-    verify();
-    let inpPeople = document.getElementById("inpPeople");
-    let dripPercentage = Number(.05);
-    inpPeople.focus();
+    dripPercentage = Number(.05);
 
-    splitter(dripPercentage);
-
+    numPeople(dripPercentage);
 }
 let bt5 = document.getElementById("bt5");
 bt5.addEventListener("click", clickbt5)
 
-
-
-
 function clickbt10() {
+    dripPercentage = .10;
+
     verify();
-    let inpPeople = document.getElementById("inpPeople");
-    let dripPercentage = .10;
-    inpPeople.focus();
-
-    splitter(dripPercentage);
-
+    numPeople(dripPercentage);
 }
 let bt10 = document.getElementById("bt10");
-bt10.addEventListener("click", clickbt10)
-
-
-
+bt10.addEventListener("click", clickbt10);
 
 function clickbt15() {
-    verify();
-    let inpPeople = document.getElementById("inpPeople");
-    let dripPercentage = .15;
-    inpPeople.focus();
+    dripPercentage = .15;
 
-    splitter(dripPercentage);
+    verify();
+    numPeople(dripPercentage);
 
 }
 let bt15 = document.getElementById("bt15");
-bt15.addEventListener("click", clickbt15)
-
-
-
+bt15.addEventListener("click", clickbt15);
 
 function clickbt25() {
-    verify();
-    let inpPeople = document.getElementById("inpPeople");
-    let dripPercentage = .25;
-    inpPeople.focus();
+    dripPercentage = .25;
 
-    splitter(dripPercentage);
+    verify();
+    numPeople(dripPercentage);
+
 }
 let bt25 = document.getElementById("bt25");
-bt25.addEventListener("click", clickbt25)
-
-
-
+bt25.addEventListener("click", clickbt25);
 
 function clickbt50() {
-    verify();
-    let inpPeople = document.getElementById("inpPeople");
     let dripPercentage = .50;
-    inpPeople.focus();
 
-    splitter(dripPercentage);
-
+    verify();
+    numPeople(dripPercentage);
 }
 let bt50 = document.getElementById("bt50");
-bt50.addEventListener("click", clickbt50)
-
-
+bt50.addEventListener("click", clickbt50);
 
 function btCustom() {
     let inCustom = document.getElementById("inCustom")
 
     let custom = Number(inCustom.value);
-    let dripPercentage = custom / 100;
+    dripPercentage = custom / 100;
 
-    splitter(dripPercentage);
-
+    verify();
+    numPeople(dripPercentage);
 }
-
-
-
-let inCustom = document.getElementById("inCustom")
+let inCustom = document.getElementById("inCustom");
 inCustom.addEventListener("keypress", function(tecla) {
     if (tecla.keyCode == 13) {
         let inpPeople = document.getElementById("inpPeople");
-        inpPeople.focus();
         btCustom();
         verify();
     }
 })
 
 
-function paymentValues() {
+
+
+//------------------
+
+//obtem o numero de pessoas
+function numPeople(dripPercentage) {
     let inpPeople = document.getElementById("inpPeople");
-    let inSpan1 = document.getElementById("span1")
-    let inSpan2 = document.getElementById("span2")
+    let numP = Number(inpPeople.value);
 
-    let numberPeople = Number(inpPeople.value);
+    if (numP == 0 || numP == "") {
+        verify();
+        numPeople(dripPercentage);
+    } else {
+        paymentValues(dripPercentage, numP);
+    }
+} 
 
-    let tripAmount =  splitter() / numberPeople;
-
-    let total = tripAmount * numberPeople;
-
-    inSpan1.textContent = "$ " + tripAmount.toFixed(2);
-    inSpan2.textContent = "$ " + total.toFixed(2);
-}
-
+let inBill = document.getElementById("inBill");
+inBill.addEventListener("keypress", function(tecla) {
+    if (tecla.keyCode == 13) {
+        verify();
+        numPeople(dripPercentage);
+    }
+})
 
 let inpPeople = document.getElementById("inpPeople");
 inpPeople.addEventListener("keypress", function(tecla) {
     if (tecla.keyCode == 13) {
-        paymentValues()
         verify();
+        numPeople(dripPercentage);
     }
 })
 
 
+//------------------
+function paymentValues(dripPercentage, numP) {
+    let tripAmount =  splitter(dripPercentage) / numP; // x = valor numero de pessoas
+    let total = tripAmount * numP;
+
+    let inSpan1 = document.getElementById("span1")
+    let inSpan2 = document.getElementById("span2")
+
+    inSpan1.textContent = "$ " + tripAmount.toFixed(2);
+    inSpan2.textContent = "$ " + total.toFixed(2);
+
+    verify();
+    if (dripPercentage == 0 || numP == 0) {
+        inSpan1.textContent = "$0.00";
+        inSpan2.textContent = "$0.00";
+    }
+
+    exibeButtom();
+}
 
 
+function exibeButtom() {
+    let btReset = document.getElementById("btReset");
 
+    btReset.className = "buttom-reset-exibe";
+}
 
-
-
-
-
-
-
-
-
-/*  let bt5 = document.getElementById("bt5");
-    let bt10 = document.getElementById("bt10");
-    let bt15 = document.getElementById("bt10");
-    let bt25 = document.getElementById("bt10");
-    let bt50 = document.getElementById("bt10");
-    let btCustom = document.getElementById("btCustom");
- */
+function reset() {
+  location.reload();
+}
+let btReset = document.getElementById("btReset");
+btReset.addEventListener("click", reset);
